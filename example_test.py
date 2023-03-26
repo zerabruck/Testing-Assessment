83996a8dd7bc79fe023f72625572999cb78b1d9b
internsAssessment <129003740+internsAssessment@users.noreply.github.com>
2023-03-26 22:37:36 +0300

    Create example_test.py

---
+++ example_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('hello'.upper(), 'HELLO')
+
+    def test_isupper(self):
+        self.assertTrue('HELLO'.isupper())
+        self.assertFalse('Hello'.isupper())
+
+if __name__ == '__main__':
+    unittest.main()
