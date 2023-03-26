cb4b02a7b46d940c26ac57c067e3a31c8fb779aa
internsAssessment <129003740+internsAssessment@users.noreply.github.com>
2023-03-26 22:39:09 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+from string_functions import *
+
+class TestToUpper(unittest.TestCase):
+    # write your code here
+    pass
+
+class TestToLower(unittest.TestCase):
+    # Write you code here
+    pass
+
+class TestCapitalize(unittest.TestCase):
+    # Write your code here
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
